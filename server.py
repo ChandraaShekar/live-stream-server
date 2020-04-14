c82773f5314fb975d5b4e4ef01e02f202a9f579b
Chandu <csreddy1998@gmail.com>
2020-04-15 01:41:24 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,114 @@
+from pyimagesearch.motion_detection import SingleMotionDetector
+from imutils.video import VideoStream
+from flask import Response
+from flask import Flask
+from flask import render_template
+import threading
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+import imagezmq
+
+
+outputFrame = None
+lock = threading.Lock()
+
+
+app = Flask(__name__)
+image_hub = imagezmq.ImageHub()
+
+time.sleep(2.0)
+
+@app.route("/")
+def index():
+	# return the rendered template
+	return render_template("index.html")
+
+def detect_motion(frameCount):
+
+	global outputFrame, lock
+
+	# initialize the motion detector and the total number of frames
+	# read thus far
+	md = SingleMotionDetector(accumWeight=0.1)
+	total = 0
+
+	# loop over frames from the video stream
+	while True:
+		rpi_name, frame = image_hub.recv_image()
+		frame = imutils.resize(frame, width=400)
+		# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		# gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+		# # grab the current timestamp and draw it on the frame
+		# timestamp = datetime.datetime.now()
+		# cv2.putText(frame, timestamp.strftime(
+		# 	"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+		# 	cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+		# # if the total number of frames has reached a sufficient
+		# # number to construct a reasonable background model, then
+		# # continue to process the frame
+		# if total > frameCount:
+		# 	# detect motion in the image
+		# 	motion = md.detect(gray)
+
+		# 	# cehck to see if motion was found in the frame
+		# 	if motion is not None:
+		# 		# unpack the tuple and draw the box surrounding the
+		# 		# "motion area" on the output frame
+		# 		(thresh, (minX, minY, maxX, maxY)) = motion
+		# 		cv2.rectangle(frame, (minX, minY), (maxX, maxY),
+		# 			(0, 0, 255), 2)
+		
+		# # update the background model and increment the total number
+		# # of frames read thus far
+		# md.update(gray)
+		# total += 1
+
+		# acquire the lock, set the output frame, and release the
+		# lock
+		with lock:
+			outputFrame = frame.copy()
+			image_hub.send_reply(b'OK')
+
+def generate():
+	# grab global references to the output frame and lock variables
+	global outputFrame, lock
+
+	# loop over frames from the output stream
+	while True:
+		# wait until the lock is acquired
+		with lock:
+			# check if the output frame is available, otherwise skip
+			# the iteration of the loop
+			if outputFrame is None:
+				continue
+
+			# encode the frame in JPEG format
+			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+
+			# ensure the frame was successfully encoded
+			if not flag:
+				continue
+		# yield the output frame in the byte format
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+			bytearray(encodedImage) + b'\r\n')
+
+@app.route("/video_feed")
+def video_feed():
+	# return the response generated along with the specific media
+	# type (mime type)
+	return Response(generate(),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+# check to see if this is the main thread of execution
+if __name__ == '__main__':
+
+	t = threading.Thread(target=detect_motion, args=(10,))
+	t.daemon = True
+	t.start()
+
+	app.run(host="0.0.0.0",port=5555,debug=True ,threaded=True, use_reloader=False)
